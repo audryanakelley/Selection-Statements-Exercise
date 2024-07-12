910761c0e0f540579d4870a7a06f875587bc8c2c
Audryana Kelley <audryanakelley@gmail.com>
2024-07-12 10:37:27 -0500

    made updates

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 33;
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput> favNumber)
+            {
+             Console.WriteLine("too high");   
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+            }
+            
         }
     }
 }
